77d99d7cbc5e6951bc352a783e6fe408c8af1aa5
Akshay T G <akshaygireesh3884@gmail.com>
2021-08-23 12:35:57 +0530

    initial commit

---
+++ src/main/java/com/example/crudapplication/controller/UserController.java
@@ -0,0 +1,52 @@
+package com.example.crudapplication.controller;
+
+
+import com.example.crudapplication.entity.User;
+import com.example.crudapplication.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @PostMapping("/addUser")
+    public User addUser(@RequestBody User user) {
+        return service.saveUser(user);
+    }
+
+    @PostMapping("/addUsers")
+    public List<User> addUsers(@RequestBody List<User> users) {
+        return service.saveUsers(users);
+    }
+
+    @GetMapping("/users")
+    public List<User> findAllUsers() {
+        return service.getUsers();
+    }
+
+    @GetMapping("/userById/{id}")
+    public User findUserById(@PathVariable int id) {
+        return service.getUserById(id);
+    }
+
+    @GetMapping("/user/{name}")
+    public User findUserByName(@PathVariable String name) {
+        return service.getUserByName(name);
+    }
+
+    @PutMapping("/update")
+    public User updateUser(@RequestBody User user) {
+        return service.updateUser(user);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteUser(@PathVariable int id) {
+        return service.deleteUser(id);
+    }
+}
+
